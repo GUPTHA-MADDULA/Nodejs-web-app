b058ecf44b83e88b2b21f8f1f3ef67d5030e3324
Rama Krishna Gupta <134220369+GUPTHA-MADDULA@users.noreply.github.com>
2025-04-07 22:44:11 +0530

    Delete tests directory

--- tests/app.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('../index');
-
-test('Should respond with a message', async () => {
-  const response = await request(app).get('/');
-  expect(response.status).toBe(200);
-  expect(response.text).toBe('Hello, CI/CD Pipeline!');
-});
